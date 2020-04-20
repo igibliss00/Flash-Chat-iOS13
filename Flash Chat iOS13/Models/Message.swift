7d89c6b67f762a5696b594b719300d98dd53c4fd
J <j>
2020-04-20 11:11:22 -0400

    completed app

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by J on 2020-04-19.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
